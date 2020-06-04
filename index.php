ef7acd2f77f225f05766788934ec688b2a73edb9
masha14013 <masha14013@rambler.ru>
2020-06-04 10:19:30 +0300

    klk

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html class="no-js" lang="ru">
+	<head>
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ 		<meta charset="utf-8">
+		<title>Эффект параллакса на css</title>
+		<link rel="stylesheet" href="/css/css.css">
+		<script type="text/javascript" src="/js/main.js" defer></script>
+		
+	</head>
+	<body>
+		<header>
+			<h1>Главный заголовок</h1>
+			<small>Подзаголовок или текст</small>
+		</header>
+	</body>
+</html>
